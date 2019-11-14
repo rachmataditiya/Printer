306563999d09afa7dca8ede8aa9a4a53d1654daf
Rachmat Aditiya <radit@arkana.co.id>
2019-11-14 07:04:28 +0700

    revert to POST

--- PrinterConsole/DataController.cs
+++ PrinterConsole/DataController.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using EmbedIO;
 using EmbedIO.Routing;
 using EmbedIO.WebApi;
@@ -22,20 +21,16 @@ namespace PrinterConsole
         private void CetakTicket(List<Ticket> data)
         {
             string tanggal = data[0].date;
-            //handle disini buat ambil data yang mau dicerak
+            //handle disini buat ambil data yang mau dicetak
         }
         public async Task UpdateStatus(int ticket_id, string api_key)
         {
             var client = new HttpClient();
-            client.BaseAddress = new Uri("http://localhost");
-            client.DefaultRequestHeaders.Add("X-Api-Key", api_key);
-            client.DefaultRequestHeaders.Accept.Add(
-                    new MediaTypeWithQualityHeaderValue("application/json"));
-            var url = "/saloka/ticket/print_proxy/" + ticket_id;
-            HttpResponseMessage response = await client.GetAsync(url);
-            response.EnsureSuccessStatusCode();
-            var resp = await response.Content.ReadAsStringAsync();
-            var result = Json.Deserialize<PrintResult>(resp);
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Put, "http://localhost/saloka/ticket/print_proxy/" +  ticket_id);
+            requestMessage.Headers.Add("X-Api-Key", api_key);
+            requestMessage.Headers.Add("Content-Type", "application/json");
+            HttpResponseMessage response = await client.SendAsync(requestMessage);
+            var result = Json.Deserialize<PrintResult>(await response.Content.ReadAsStringAsync());
             GetResult(result);
         }
 
